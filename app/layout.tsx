8a966f7c9fd467b7be35913144618b5c4acf3d70
Damian <damian_l@hotmail.es>
2023-03-14 13:14:43 -0300

    iwon-page-version-1.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,34 @@
-import NavBar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Image from 'next/image'
-import '../styles/output.css'
-import  { Poppins }  from "next/font/google"
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Image from "next/image";
+import "../styles/output.css";
+import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
-    weight: "500",
-    subsets: ['latin']
-})
+  weight: "500",
+  subsets: ["latin"],
+});
 
-export default function RootLayout({ children }: {
-    children: React.ReactNode
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang='es'>
-            <body className={`bg-[#2c2c2c] ${poppins.className}`}>
-                <NavBar />
-                <main className='mx-auto flex items-center justify-center w-[600px] p] xl:w-auto h-screen'>{children}</main>
-                {/* <Footer /> */}
-            </body>
-        </html>
-    )
+  return (
+    <html lang="es">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700&family=Roboto:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className={`bg-darkGray ${poppins.className}`}>
+        <NavBar />
+        <main className="mx-auto xl:w-auto">{children}</main>
+        {/* <Footer /> */}
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,81 +1,43 @@
-import { url } from "inspector";
-import { Roboto_Serif, Montserrat } from "next/font/google";
-import { BsWhatsapp } from "react-icons/bs";
-
-const roboto_serif = Roboto_Serif({
-  subsets: ["latin"],
-  weight: "300",
-});
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: "800",
-});
-
-const montserrat_light = Montserrat({
-  subsets: ["latin"],
-  weight: "300",
-});
+"use client";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Works from "../components/Works";
+import { useEffect, useState } from "react";
+import Footer from "../components/Footer";
+import observer from "../utils/observer";
 
 export default function Home() {
+  const [video, setVideo] = useState("/videos/hero-video.mp4");
+
+  document.addEventListener("scroll", () => {
+    if(scrollY >= 1080) {
+      setVideo("/videos/video-prueba-1.mp4");
+    } else {
+      setVideo("/videos/hero-video.mp4")
+    }
+
+    console.log(scrollY)
+  })
+
   return (
-    <div className="flex items-center justify-center gap-[200px] w-full">
-      {/* background video */}
+    <div className="flex flex-col">
       <video
-        className="z-[-10] top-[-50%] fixed w-[1920px] h-[1080px] blur-[100px]"
-        src="/hero-video.mp4"
+        className="fixed z-[-10]
+            xl:h-[1080px] xl:w-[1920px]"
+        src={video}
         autoPlay
         muted
         loop
       ></video>
-      <video
-        className="z-[-10] fixed w-[1920px] h-[1080px]"
-        src="/hero-video.mp4"
-        autoPlay
-        muted
-        loop
-      ></video>
-      <video
-        className="z-[-20] fixed bottom-[-50%] w-[1920px] h-[1080px] blur-[100px]"
-        src="/hero-video.mp4"
-        autoPlay
-        muted
-        loop
-      ></video>
-      {/* main text */}
-      <div className="flex flex-col gap-[20px] items-center w-[600px] xl:w-[1275px]
-      xl:mt-0 mt-[150px]">
-        <h1
-          className={`leading-[90px] text-center text-[#fff] text-[64px] ${montserrat.className}`}
-        >
-          Participa En Nuestro <br />
-          <span className="bg-[#5FFC7B] text-[#2c2c2c] py-2 px-4">
-            Sorteo De Videoclips
-          </span>{" "}
-          <br />
-          En Calidad De Cine.
-        </h1>
-        <p
-          className={`text-[16px] text-center text-[#aaaaaa] w-[90%] ${montserrat_light.className}`}
-        >
-          Envia a nuestro WhatsApp un mensaje diciendo "QUIERO PARTICIPAR".
-        </p>
-        <a
-          href="#"
-          className={`text-[24px] text-[#2c2c2c]
-         px-[45px] py-[22.5px] 
-         bg-[#5FFC7B] shadow-[0_11px_19px_#222222d9] 
-         w-max rounded-[40px] my-[20px] flex items-center gap-[20px] ${montserrat.className}`}
-        >
-          <BsWhatsapp size={"30px"} color={"#2c2c2c"} />{" "}
-          <span className="text-[#2c2c2c]">QUIERO PARTICIPAR</span>
-        </a>
-      </div>
-      {/* <div className="flex w-[40%] h-[400px] justify-center items-start">
-        <video id="hero-video" className="rounded-[30px]" autoPlay muted>
-          <source src="/hero-video.mp4" type="video/mp4"/>
-        </video>
-      </div> */}
+      {/* hero */}
+      <Hero />
+      {/* producciones */}
+      <Features />
+      <Works
+        song="Matamo' La Calle"
+        artist="Callejero"
+      />
+      <Footer />
     </div>
   );
 }
