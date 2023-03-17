c03d07ada0468dc9489bc62f4496e0224fed4579
Damian <damian_l@hotmail.es>
2023-03-16 21:07:36 -0300

    iwon-prods-version-1

--- components/Video.tsx
+++ components/Video.tsx
@@ -4,6 +4,7 @@ import { playVideo, pauseVideo } from "../utils/handleVideo";
 export default function Video({ src }: any) {
   return (
     <video
+    
       data-video
       className="rounded-[30px] transition duration-1000 hover:scale-110"
       width={400}
